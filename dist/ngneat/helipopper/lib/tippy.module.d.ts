131720fc1aa34609340eb17301c2cdefecb3a496
danil <blnvdanil@gmail.com>
2021-11-12 23:18:50 +0300

    test

--- dist/ngneat/helipopper/lib/tippy.module.d.ts
+++
@@ -1,5 +0,0 @@
-import { ModuleWithProviders } from '@angular/core';
-import { TippyConfig } from './tippy.types';
-export declare class TippyModule {
-    static forRoot(config?: Partial<TippyConfig>): ModuleWithProviders<TippyModule>;
-}
--- dist/ngneat/helipopper/ngneat-helipopper.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Generated bundle index. Do not edit.
- */
-export * from './public-api';
-export { TippyConfig as ɵa } from './lib/tippy.types';
